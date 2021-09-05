5738d756a9e08c14a4eeb3dbf53f25e8207d692d
Christian Anorga <anorga2990@gmail.com>
2021-09-04 20:45:01 -0700

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,45 +6,45 @@ import jjk from './assets/jjk.jpg';
 
 function Home() {
     return (
-        <Container style={{ textAlign: 'left' }}>
+        <Container>
             {/* Card One */}
-            <div className="card mb-3" style={{ marginTop: 20 }}>
+            <div className="card mb-3" style={{ marginTop: 20, maxWidth:800 }}>
                 <div className="row g-0">
-                    <div className="col">
+                    <div className="col" style={{ textAlign:'left' }}>
                         <img src={attack} className="img-fluid rounded-start" alt="Attack on Titan Manga Cover" style={{ width: 269, height: 300 }} />
                     </div>
-                    <div className="col">
+                    <div className="col" style={{ paddingRight:50}}>
                         <div className="card-body">
                             <h5 className="card-title">Attack on Titan</h5>
-                            <p className="card-text">Several hundred years ago, humans were nearly exterminated by titans.</p>
+                            <p className="card-text">After his hometown is destroyed and his mother is killed, young Eren Jaeger vows to cleanse the earth of the giant humanoid Titans that have brought humanity to the brink of extinction.</p>
                         </div>
                     </div>
                 </div>
             </div>
             {/* Card Two */}
-            <div className="card mb-3">
+            <div className="card mb-3" style={{ marginTop: 20, maxWidth:800 }}>
                 <div className="row g-0">
-                    <div className="col">
+                    <div className="col" style={{ textAlign:'left' }}>
                         <img src={chainsaw} className="img-fluid rounded-start" alt="Chainsaw Man Manga Cover" style={{ width: 269, height: 300 }} />
                     </div>
-                    <div className="col">
+                    <div className="col" style={{ paddingRight:50}}>
                         <div className="card-body">
                             <h5 className="card-title">Chainsaw Man</h5>
-                            <p className="card-text">The story is set in a world where Devils cause harm to humans, which makes them a target for extermination.</p>
+                            <p className="card-text">When his father died, Denji was stuck with a huge debt and no way to pay it back. Thanks to a Devil dog he saved named Pochita, he's able to survive through odd jobs and killing Devils for the Yakuza.</p>
                         </div>
                     </div>
                 </div>
             </div>
             {/* Card Three */}
-            <div className="card mb-3">
+            <div className="card mb-3" style={{ marginTop: 20, maxWidth:800 }}>
                 <div className="row g-0">
-                    <div className="col">
+                    <div className="col" style={{ textAlign:'left' }}>
                         <img src={jjk} className="img-fluid rounded-start" alt="Jujutsu Kaisen Manga Cover" style={{ width: 269, height: 300 }} />
                     </div>
-                    <div className="col">
+                    <div className="col" style={{ paddingRight:50}}>
                         <div className="card-body">
                             <h5 className="card-title">Jujutsu Kaisen</h5>
-                            <p className="card-text">Yuuji is a genius at track and field. But he has zero interest running around in circles, he’s happy as a clam in the Occult Research Club. Although he’s only in the club for kicks, things get serious when a real spirit shows up at school!</p>
+                            <p className="card-text">The story of Jujutsu Kaisen is set in a world where Cursed Spirits feed on unsuspecting humans and fragments of the legendary and feared demon Ryomen Sukuna have been lost and scattered about.</p>
                         </div>
                     </div>
                 </div>
