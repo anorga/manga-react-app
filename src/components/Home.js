3dda125dab94a67a7522e4e5eac3a8d485bff65e
Christian Anorga <anorga2990@gmail.com>
2021-09-04 18:39:54 -0700

    updated Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,10 +5,10 @@ function Home() {
     return (
         <Container style={{ textAlign: 'left' }}>
             {/* Card One */}
-            <div className="card mb-3" style={{ maxWidth: '540px' }}>
+            <div className="card mb-3" >
                 <div className="row g-0">
                     <div className="col">
-                        <img src="images/attack.png" className="img-fluid rounded-start" alt="Attack on Titan logo" style={{ width: 269, height: 300 }} />
+                        <img src="images/attack.png" className="img-fluid rounded-start" alt="Attack on Titan Manga Cover" style={{ width: 269, height: 300 }} />
                     </div>
                     <div className="col">
                         <div className="card-body">
@@ -22,7 +22,7 @@ function Home() {
             <div className="card mb-3" style={{ maxWidth: '540px' }}>
                 <div className="row g-0">
                     <div className="col">
-                        <img src="images/chainsaw.jpeg" className="img-fluid rounded-start" alt="..." style={{ width: 269, height: 300 }} />
+                        <img src="images/chainsaw.jpeg" className="img-fluid rounded-start" alt="Chainsaw Man Manga Cover" style={{ width: 269, height: 300 }} />
                     </div>
                     <div className="col">
                         <div className="card-body">
@@ -35,14 +35,13 @@ function Home() {
             {/* Card Three */}
             <div className="card mb-3" style={{ maxWidth: '540px' }}>
                 <div className="row g-0">
-                    <div className="col-md-4">
-                        <img src="..." className="img-fluid rounded-start" alt="..." />
+                    <div className="col">
+                        <img src="images/jjk.jpg" className="img-fluid rounded-start" alt="Jujutsu Kaisen Manga Cover" />
                     </div>
-                    <div className="col-md-8">
+                    <div className="col">
                         <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                            <h5 className="card-title">Jujutsu Kaisen</h5>
+                            <p className="card-text">Yuuji is a genius at track and field. But he has zero interest running around in circles, he’s happy as a clam in the Occult Research Club. Although he’s only in the club for kicks, things get serious when a real spirit shows up at school!</p>
                         </div>
                     </div>
                 </div>
