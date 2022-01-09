02a60760c9fae91128adf3bb770c0b2f64bebbf7
Christian Anorga <anorga2990@gmail.com>
2022-01-09 12:39:47 -0800

    replaced home code
    replaced cards for list items

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,60 +9,47 @@ import { LinkContainer } from "react-router-bootstrap";
 function Home() {
     return (
         <Container>
-            {/* Card One */}
-            <div className="card mb-3" style={{ marginTop: 20, maxWidth: 800, maxHeight: 300 }}>
-                <div className="row g-0">
+            <ul className="list-group list-main">
+                {/* Card One */}
+                <li className="list-group-item manga-li" >
                     <LinkContainer to="/attack">
-                        <div className="col" style={{ textAlign: 'left' }}>
-                            <img src={attack} id="attack" className="img-fluid rounded-start" alt="Attack on Titan Manga Cover" style={{ width: 269, height:290 }} />
-                        </div>
-                    </LinkContainer>
-                    <LinkContainer to="/attack" style={{ paddingRight: 50 }}>
-                        <div className="col">
-                            <div className="card-body">
-                                <h5 className="card-title" style={{ paddingTop: 5 }}>Attack on Titan</h5>
+                        <div className="mangaList">
+                            <img src={attack} className="img-fluid rounded-start homeImage" alt="Attack on Titan Manga Cover" />
+                            <div className="homeText">
+                                <h5 className="card-title">Attack on Titan</h5>
                                 <p className="card-text">After his hometown is destroyed and his mother is killed, Eren Jaeger vows to cleanse the earth of the giant humanoid Titans that have brought humanity to the brink of extinction.</p>
                             </div>
                         </div>
                     </LinkContainer>
-                </div>
-            </div>
-            {/* Card Two */}
-            <div className="card mb-3" style={{ maxWidth: 800, maxHeight: 300 }}>
-                <div className="row g-0">
+                </li>
+
+                {/* Card Two */}
+                <li className="list-group-item manga-li">
                     <LinkContainer to="/chainsaw">
-                        <div className="col" style={{ textAlign: 'left' }}>
-                            <img src={chainsaw} className="img-fluid rounded-start" alt="Chainsaw Man Manga Cover" style={{ width: 269, height:290 }} />
-                        </div>
-                    </LinkContainer>
-                    <LinkContainer to="/chainsaw" style={{ paddingRight: 50 }}>
-                        <div className="col">
-                            <div className="card-body">
-                                <h5 className="card-title" style={{ paddingTop: 5 }}>Chainsaw Man</h5>
+                        <div className="mangaList">
+                            <img src={chainsaw} className="img-fluid rounded-start homeImage" alt="Chainsaw Man Manga Cover" />
+                            <div className="homeText">
+                                <h5 className="card-title">Chainsaw Man</h5>
                                 <p className="card-text">When his father died, Denji was stuck with a huge debt. Thanks to a Devil dog he saved named Pochita, he's able to survive through odd jobs and killing Devils for the Yakuza.</p>
                             </div>
                         </div>
                     </LinkContainer>
-                </div>
-            </div>
-            {/* Card Three */}
-            <div className="card mb-3" style={{ maxWidth: 800, maxHeight: 300 }}>
-                <div className="row g-0">
+                </li>
+
+                {/* Card Three */}
+                <li className="list-group-item manga-li">
                     <LinkContainer to="/jujutsu">
-                        <div className="col" style={{ textAlign: 'left' }}>
-                            <img src={jjk} className="img-fluid rounded-start" alt="Jujutsu Kaisen Manga Cover" style={{ width: 269, height:290 }} />
-                        </div>
-                    </LinkContainer>
-                    <LinkContainer to="/jujutsu" style={{ paddingRight: 50 }}>
-                        <div className="col">
-                            <div className="card-body">
-                                <h5 className="card-title" style={{ paddingTop: 5 }}>Jujutsu Kaisen</h5>
+                        <div className="mangaList">
+                            <img src={jjk} className="img-fluid rounded-start homeImage" alt="Jujutsu Kaisen Manga Cover" />
+                            <div className="homeText">
+                                <h5 className="card-title">Jujutsu Kaisen</h5>
                                 <p className="card-text">Set in a world where Cursed Spirits feed on unsuspecting humans and fragments of the demon Ryomen Sukuna have been lost and scattered about.</p>
                             </div>
                         </div>
                     </LinkContainer>
-                </div>
-            </div>
+                </li>
+
+            </ul>
         </Container>
     );
 }
