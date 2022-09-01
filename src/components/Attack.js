54555dcb7a050e47a386607d75065c547d3e997a
Christian Anorga <anorga2990@gmail.com>
2022-09-01 15:34:26 -0700

    updated manga links

--- src/components/Attack.js
+++ src/components/Attack.js
@@ -7,31 +7,31 @@ function Attack() {
         <Container>
         <img src={attackBanner} className="img-fluid" alt="Attack on Titan banner" style={{ borderRadius: 4 }} />
         <ul className="list-group" style={{ marginTop: 10 }}>
-        <li className="list-group-item">Chapter 117<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-1/">Read</a></span></li>
-        <li className="list-group-item">Chapter 118<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-2/">Read</a></span></li>
-        <li className="list-group-item">Chapter 119<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-3/">Read</a></span></li>
-        <li className="list-group-item">Chapter 120<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-4/">Read</a></span></li>
-        <li className="list-group-item">Chapter 121<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-5/">Read</a></span></li>
-        <li className="list-group-item">Chapter 122<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-6/">Read</a></span></li>
-        <li className="list-group-item">Chapter 123<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-7/">Read</a></span></li>
-        <li className="list-group-item">Chapter 124<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-8/">Read</a></span></li>
-        <li className="list-group-item">Chapter 125<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-9/">Read</a></span></li>
-        <li className="list-group-item">Chapter 126<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-10/">Read</a></span></li>
-        <li className="list-group-item">Chapter 127<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-11/">Read</a></span></li>
-        <li className="list-group-item">Chapter 128<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-12/">Read</a></span></li>
-        <li className="list-group-item">Chapter 129<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-13/">Read</a></span></li>
-        <li className="list-group-item">Chapter 130<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-14/">Read</a></span></li>
-        <li className="list-group-item">Chapter 131<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-15/">Read</a></span></li>
-        <li className="list-group-item">Chapter 132<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-16/">Read</a></span></li>
-        <li className="list-group-item">Chapter 133<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-17/">Read</a></span></li>
-        <li className="list-group-item">Chapter 134<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-18/">Read</a></span></li>
-        <li className="list-group-item">Chapter 135<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-19/">Read</a></span></li>
-        <li className="list-group-item">Chapter 136<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-20/">Read</a></span></li>
-        <li className="list-group-item">Chapter 137<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-21/">Read</a></span></li>
-        <li className="list-group-item">Chapter 138<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-22/">Read</a></span></li>
-        <li className="list-group-item">Chapter 139<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-23/">Read</a></span></li>
-        <li className="list-group-item">Chapter 139.5<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-24/">Read</a></span></li>
-        <li className="list-group-item">Chapter 140<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-25/">Read</a></span></li>
+        <li className="list-group-item">Chapter 1<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-1/">Read</a></span></li>
+        <li className="list-group-item">Chapter 2<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-2/">Read</a></span></li>
+        <li className="list-group-item">Chapter 3<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-3/">Read</a></span></li>
+        <li className="list-group-item">Chapter 4<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-4/">Read</a></span></li>
+        <li className="list-group-item">Chapter 5<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-5/">Read</a></span></li>
+        <li className="list-group-item">Chapter 6<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-6/">Read</a></span></li>
+        <li className="list-group-item">Chapter 7<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-7/">Read</a></span></li>
+        <li className="list-group-item">Chapter 8<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-8/">Read</a></span></li>
+        <li className="list-group-item">Chapter 9<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-9/">Read</a></span></li>
+        <li className="list-group-item">Chapter 10<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-10/">Read</a></span></li>
+        <li className="list-group-item">Chapter 11<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-11/">Read</a></span></li>
+        <li className="list-group-item">Chapter 12<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-12/">Read</a></span></li>
+        <li className="list-group-item">Chapter 13<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-13/">Read</a></span></li>
+        <li className="list-group-item">Chapter 14<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-14/">Read</a></span></li>
+        <li className="list-group-item">Chapter 15<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-15/">Read</a></span></li>
+        <li className="list-group-item">Chapter 16<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-16/">Read</a></span></li>
+        <li className="list-group-item">Chapter 17<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-17/">Read</a></span></li>
+        <li className="list-group-item">Chapter 18<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-18/">Read</a></span></li>
+        <li className="list-group-item">Chapter 19<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-19/">Read</a></span></li>
+        <li className="list-group-item">Chapter 20<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-20/">Read</a></span></li>
+        <li className="list-group-item">Chapter 21<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-21/">Read</a></span></li>
+        <li className="list-group-item">Chapter 22<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-22/">Read</a></span></li>
+        <li className="list-group-item">Chapter 23<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-23/">Read</a></span></li>
+        <li className="list-group-item">Chapter 24<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-24/">Read</a></span></li>
+        <li className="list-group-item">Chapter 25<span style={{float: 'right'}} className="badge rounded-pill bg-primary"><a href="https://readaot.com/manga/shingeki-no-kyojin-chapter-25/">Read</a></span></li>
       </ul>
       </Container>
     );
