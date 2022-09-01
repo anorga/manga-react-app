0c1676e200b9b0b4534f17d8c7c0056e34d57ae3
Christian Anorga <anorga2990@gmail.com>
2022-09-01 16:36:59 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,12 +5,13 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
-
