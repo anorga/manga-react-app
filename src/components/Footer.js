d0379454f42005cc3dcd702c557311dac100c34e
Christian Anorga <anorga2990@gmail.com>
2022-09-09 15:24:16 -0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import email from './assets/email.png';
 
 function Footer() {
     return (
-        <Container style={{ marginTop: 20, marginBottom: 10, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <Container style={{ marginTop: 70, marginBottom: 10, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
             <a href="https://github.com/anorga" target="_blank">
                 <img
                     src={github}
@@ -17,8 +17,8 @@ function Footer() {
             <a href="mailto: anorga2990@gmail.com">
                 <img
                     src={email}
-                    width="24"
-                    height="24"
+                    width="25"
+                    height="25"
                     style={{ marginRight: 10 }}
                     alt="Email logo"
                 />
