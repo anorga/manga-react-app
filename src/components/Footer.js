52585cdc2e6350e507461371c70fffe49458c032
Christian Anorga <anorga2990@gmail.com>
2022-09-09 14:56:34 -0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,19 +4,22 @@ import github from './assets/github.png';
 
 function Footer() {
     return (
-        <Container style={{ textAlign: "left", marginTop: 20, marginBottom: 10 }}>
-            <a href = "https://github.com/anorga">
-                <img
-                src={github}
-                width="41"
-                height="41"
-                className="d-inline-block"
-                alt="React Bootstrap logo"
-            />
-            </a>{' '}&#169; 2022 Christian Anorga
-        </Container>
-
+        <React.Fragment>
+            <Container style={{ textAlign: "left", marginTop: 20, marginBottom: 10 }}>
+                <a href="https://github.com/anorga">
+                    <img
+                        src={github}
+                        width="41"
+                        height="41"
+                        className="d-inline-block"
+                        alt="React Bootstrap logo"
+                    />
+                </a>
+            </Container>
+            {' '}&#169; 2022 Christian Anorga
+        </React.Fragment>
     );
+
 }
 
 export default Footer;
